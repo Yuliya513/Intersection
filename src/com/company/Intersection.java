04dda766b9d380bb9123f4d4c563630728448941
Yuliya513 <yuliya.senik@gmail.com>
2021-06-11 13:19:21 +0300

    Initial commit

---
+++ src/com/company/Intersection.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Intersection {
+
+    public static void main(String[] args) {
+        public class Intersection Two Hash Sets{
+            public static void main (String [] args){
+                Set <Integer> set1=new HashSet<>();
+                set1.add(1);
+                set1.add(2);
+                set1.add(1);
+                set1.add(1);
+                set<Integer>set2=new HashSet<>();
+                set2. add(1);
+                set2.add(2);
+                set2.add(3);
+                set2.add(4);
+                set2.add(5);
+                aet1.retain All(set2);
+                System.out.println(set1);
+
+            }
+        }
+    }
+
+    List<Set<Integer>>list=new Array List<>();
+    list.add(set1);
+    list.add(set2);
+    list.add(set3);
+    for (int i=1; i<list.size()); i++)
+
+    {
+        firstSet.retainAll(list.get(i));
+    }
+    System.out.println(firstSrt);
+    }
+}
